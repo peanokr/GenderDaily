790555ffa1713785ee23a5f6e35273fdf3a87afd
peanokr <wasdodo@gmail.com>
2016-12-16 13:28:57 -0700

    fixed the button

--- app/src/main/java/com/companynamehere/wasdo/genderdaily/MainMenu.java
+++ app/src/main/java/com/companynamehere/wasdo/genderdaily/MainMenu.java
@@ -2,6 +2,7 @@ package com.companynamehere.wasdo.genderdaily;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,7 +12,8 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         System.out.println("cant stop wont stop");
     }
-    public void changeScreen(){
+
+    public void changeScreen(View view) {
         setContentView(R.layout.activity_userinfo_list);
     }
 }
